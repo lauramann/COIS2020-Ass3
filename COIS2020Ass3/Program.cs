bbfbb09dc9160e47990a79598c15d24635850024
lauramann <lauramann@trentu.ca>
2016-11-27 14:02:55 -0500

    rough

--- COIS2020Ass3/Program.cs
+++ COIS2020Ass3/Program.cs
@@ -13,7 +13,7 @@ namespace COIS2020Ass3
 	class Node
 	{
 		public string directory;
-		public List<string> file; //need to define List somewhere else by saying file = new List<string>
+		public List<string> file = new List<string>; //need to define List somewhere else by saying file = new List<string>
 		public Node leftMostChild;
 		public Node rightMostSibling;
 
@@ -21,6 +21,7 @@ namespace COIS2020Ass3
 
 	public class FileSystem
 	{
+
 		private Node root;
 
 		//Creates a file system with a root directory
@@ -32,29 +33,34 @@ namespace COIS2020Ass3
 		//returns false if the file already exists or the path is undefined; true otherwise
 		public bool AddFile(string address)
 		{
+			return true;
 		}
 
 		//removes the file at the given address
 		//Returns flase if the file is not found or the path is undefined; true otherwise
 		public bool RemoveFile(string address)
 		{
+			return true;
 		}
 
 		//adds a directory at the given address
 		//returns false if the directory already exists or the path is undefined; true otherwise
 		public bool AddDirectory(string address)
 		{
+			return true;
 		}
 
 		//removes the directory (and its subdirectories) at the given address
 		//Returns flase if the directory is not found or the path is undefined; true otherwise
 		public bool RemoveDirectory(string address)
 		{
+			return true;
 		}
 
 		//returns the number of files in the file system
 		public int NumberFiles()
 		{
+			return 0;
 		}
 
 		//prints the directories in a pre-order fashion along with their files
